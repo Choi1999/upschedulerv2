2a99a41f20dc73a319aa79402fba8fe8cc283bf4
Choi1999 <dprtldk157@gmail.com>
2024-09-27 10:52:01 +0900

    init

---
+++ src/main/java/com/sparta/upschedulerv2/Upschedulerv2Application.java
@@ -0,0 +1,13 @@
+package com.sparta.upschedulerv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Upschedulerv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Upschedulerv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/upschedulerv2/Upschedulerv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.upschedulerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Upschedulerv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
